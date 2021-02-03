8668539b986576dbb76c00d103b8d76a130caeb4
Emmanuel Nwachi <egreatness1@gmail.com>
2021-02-03 09:22:16 +0100

    sidebar updates

--- src/layout-components/Sidebar/navItems.js
+++ src/layout-components/Sidebar/navItems.js
@@ -44,59 +44,39 @@ export default [
     content: JSON.parse(
       `[
   {
-    "label": "Dashboards",
+    "label": "Home",
     "icon": "DashboardTwoToneIcon",
     "content": [
       {
-        "label": "Default",
+        "label": "Home",
         "description": "This is a dashboard page example built using this template.",
         "to": "/DashboardDefault"
       }
     ]
   },
   {
-    "label": "Elements",
+    "label": "Messages",
     "icon": "SettingsIcon",
     "content": [
-      {
-        "label": "Buttons",
-        "description": "Wide selection of buttons that feature different styles for backgrounds, borders and hover options!",
-        "to": "/Buttons"
+           {
+        "label": "Inbox",
+        "description": "Add scrolling areas to any elements with custom scrollbars or default browser ones.",
+        "to": "/Scrollable"
+      },
+              {
+        "label": "Sent Messages",
+        "description": "Add scrolling areas to any elements with custom scrollbars or default browser ones.",
+        "to": "/Scrollable"
       },
       {
-        "label": "Dropdowns",
-        "description": "A drop-down list is a graphical control element, similar to a list box, that allows the user to choose one value from a list.",
-        "to": "/Dropdowns"
+        "label": "Draft",
+        "description": "Basic and dynamic pagination for use in your next awesome application.",
+        "to": "/Pagination"
       },
       {
         "label": "Navigation menus",
         "description": "Navigation menus are one of the basic building blocks for any web or mobile app.",
         "to": "/NavigationMenus"
-      },
-      {
-        "label": "Progress Bars",
-        "description": "You can use the progress bars on their own or in combination with other widgets.",
-        "to": "/ProgressBars"
-      },
-      {
-        "label": "Pagination",
-        "description": "Basic and dynamic pagination for use in your next awesome application.",
-        "to": "/Pagination"
-      },
-      {
-        "label": "Scrollable",
-        "description": "Add scrolling areas to any elements with custom scrollbars or default browser ones.",
-        "to": "/Scrollable"
-      },
-      {
-        "label": "Badges",
-        "description": "Badges and labels are used to offer extra small pieces of info for your content.",
-        "to": "/Badges"
-      },
-      {
-        "label": "Icons",
-        "description": "Wide icons selection including from flag icons to FontAwesome and other icons libraries.",
-        "to": "/Icons"
       },
       {
         "label": "Utilities & Helpers",
@@ -106,18 +86,18 @@ export default [
     ]
   },
   {
-    "label": "Cards",
+    "label": "Transactions",
     "icon": "ViewModuleIcon",
     "content": [
       {
-        "label": "Cards examples 3",
+        "label": "All Transactions",
         "description": "Wide selection of cards with multiple styles, borders, actions and hover effects.",
         "to": "/Cards3"
       }
     ]
   },
   {
-    "label": "Presentation Blocks",
+    "label": "Contacts",
     "icon": "ErrorIcon",
     "content": [
       {
@@ -127,39 +107,9 @@ export default [
       }
     ]
   },
+
   {
-    "label": "Widgets",
-    "icon": "ReceiptIcon",
-    "content": [
-      {
-        "label": "Accordions",
-        "description": "Accordions represent collapsable component with extended functionality.",
-        "to": "/Accordions"
-      },
-      {
-        "label": "Modal dialogs",
-        "description": "Wide selection of modal dialogs styles and animations available.",
-        "to": "/Modals"
-      },
-      {
-        "label": "Notifications",
-        "description": "Show beautiful, animated growl like notifications or alerts on your pages screens.",
-        "to": "/Notifications"
-      },
-      {
-        "label": "Popovers",
-        "description": "Add small overlay content, like those found in iOS, to any element for housing secondary information.",
-        "to": "/Popovers"
-      },
-      {
-        "label": "Tabs",
-        "description": "Tabs are used to split content between multiple sections. Wide variety available.",
-        "to": "/Tabs"
-      }
-    ]
-  },
-  {
-    "label": "Regular Tables",
+    "label": "My Lists",
     "icon": "CodeIcon",
     "content": [
       {
@@ -175,7 +125,7 @@ export default [
     ]
   },
   {
-    "label": "Forms Elements",
+    "label": "Orders",
     "icon": "BarChartIcon",
     "content": [
       {
