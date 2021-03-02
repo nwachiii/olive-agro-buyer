cbf0c4239bf9453722bb38d5956aef9201934401
Emmanuel Nwachi <egreatness1@gmail.com>
2021-03-02 19:03:50 +0100

    Connect to store

---
+++ src/redux/product/productActions.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import {
+  FETCH_PRODUCTS_REQUEST,
+  FETCH_PRODUCTS_SUCCESS,
+  FETCH_PRODUCTS_FAILURE,
+} from "./productTypes";
+
+export const fetchUsers = () => {
+  return (dispatch) => {
+    dispatch(fetchProductsRequest());
+    axios
+      .get("https://www.api.oliveagro.org/api/product/list/all")
+      .then((response) => {
+        // response.data is the products
+        const products = response.data;
+        dispatch(fetchProductsSuccess(products));
+      })
+      .catch((error) => {
+        // error.message is the error message
+        dispatch(fetchProductsFailure(error.message));
+      });
+  };
+};
+
+export const fetchProductsRequest = () => {
+  return {
+    type: FETCH_PRODUCTS_REQUEST,
+  };
+};
+
+export const fetchProductsSuccess = (products) => {
+  return {
+    type: FETCH_PRODUCTS_SUCCESS,
+    payload: products,
+  };
+};
+
+export const fetchProductsFailure = (error) => {
+  return {
+    type: FETCH_PRODUCTS_FAILURE,
+    payload: error,
+  };
+};
--- src/redux/rootReducer/index.js
+++ src/redux/rootReducer/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import ThemeOptions from "./ThemeOptions";
+import productReducer from "../product/productReducer";
 
 export default combineReducers({
   ThemeOptions,
+  product: productReducer,
 });
